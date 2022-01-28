7375da524592e9429d8ccfb37ea562b0287b8a88
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-01-27 23:39:29 -0500

    commit tree 7b94298619e0d0ef43b19c9853e641d966858d71

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
+const { objectContaining } = require("expect");
+
 // Code your solution here
+let drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(array, element){
+ return array.filter( name => name.toLowerCase() === element.toLowerCase())
+}
+
+function fuzzyMatch (array, element) {
+    function newArray(name) {
+        return name.substring(0, element.length) === element.substring(0,element.length)
+    }
+    return array.filter(newArray)
+    // return array.filter(name => name.substring(0,element.length) === element.substring(0,element.length))
+}
+
+
+
+
+
+
+function  matchName(array, string) {
+    const drivers = [
+        {
+          name: 'Bobby',
+          hometown: 'Pittsburgh' },
+        {
+          name: 'Sammy',
+          hometown: 'New York' } ,
+        {
+          name: 'Sally',
+          hometown: 'Cleveland' },
+        {
+          name: 'Annette',
+          hometown: 'Los Angeles' },
+        {
+          name: 'Bobby',
+          hometown: 'Tampa Bay' }
+      ];
+
+
+    let newArray = array.filter(function(element){
+    return element.name === string;
+    });
+
+    return newArray;
+}
+    
+    
+
+
+
